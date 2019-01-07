372114216f261f5612fbc750d89868a2749080bd
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-01-07 13:18:15 -0500

    integrated firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyD4VaYHIpKVy6gE7tp6GHY5sz4O-mf_JvM",
+  authDomain: "bloc-chat-91f75.firebaseapp.com",
+  databaseURL: "https://bloc-chat-91f75.firebaseio.com",
+  projectId: "bloc-chat-91f75",
+  storageBucket: "bloc-chat-91f75.appspot.com",
+  messagingSenderId: "550496973381"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
