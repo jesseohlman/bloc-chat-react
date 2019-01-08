338e3f69e883be51290aab5b7e85c8530a77f833
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-01-08 10:11:11 -0500

    working on firebase problems

--- src/App.js
+++ src/App.js
@@ -5,41 +5,36 @@ import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import { BrowserRouter } from 'react-router-dom'
 
-   // Initialize Firebase
+  // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyD4VaYHIpKVy6gE7tp6GHY5sz4O-mf_JvM",
-    authDomain: "bloc-chat-91f75.firebaseapp.com",
-    databaseURL: "https://bloc-chat-91f75.firebaseio.com",
-    projectId: "bloc-chat-91f75",
-    storageBucket: "bloc-chat-91f75.appspot.com",
-    messagingSenderId: "550496973381"
+    apiKey: "AIzaSyBsRjFfGzk2G2ezQ2cUU79ah-5CRiC39eA",
+    authDomain: "bloc-chat-2-3b676.firebaseapp.com",
+    databaseURL: "https://bloc-chat-2-3b676.firebaseio.com",
+    projectId: "bloc-chat-2-3b676",
+    storageBucket: "bloc-chat-2-3b676.appspot.com",
+    messagingSenderId: "367123375755"
   };
   firebase.initializeApp(config);
 
-class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-      <header>
-        <div className="columns is-centered is-full">
-        <nav className="navbar">
-        
-        </nav>
+ class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header>
+            <div className="columns is-centered is-full">
+              <nav className="navbar" />
+            </div>
+            <h1 className="title has-text-centered is-2 has-text-success">
+              Bloc Chat
+            </h1>
+            <main>
+              <RoomList firebase={firebase} />
+            </main>
+          </header>
         </div>
-<h1 className="title has-text-centered is-2 has-text-success">Bloc Jams</h1>
-<main>
-<BrowserRouter>
-<div>
-<Route exact path="/" component={RoomList} />
-</div>
-</BrowserRouter>
-</main>
-      </header>
-      <RoomList firebase={this.firebase}/>
-      </div>
-    );
+      );
+    }
   }
-}
 
 export default App;
